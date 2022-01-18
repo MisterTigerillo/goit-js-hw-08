0a4a70d860373e454c5e47d203f0f815585852da
Dmytro Gobosh <irulka103@ukr.net>
2022-01-18 23:46:33 +0200

    + files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+const Lightbox = require('simplelightbox');
 // Change code below this line
+const gallery_js = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+let instance;
 
-console.log(galleryItems);
+gallery_js.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery_js.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  window.addEventListener('keydown', onEscapePress);
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  instance = basicLightbox.create(`<img src="${evt.target.dataset.source}">`);
+  instance.show();
+}
+
+function onEscapePress(evt) {
+  if (evt.code === 'Escape') {
+    instance.close();
+    window.removeEventListener('keydown', onEscapePress);
+  }
+  console.log(evt);
+}
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+// console.log(galleryItems);
